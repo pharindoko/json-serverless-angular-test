3f54cdce0a863b92c7665dfaeec7e4e5a4e3b99b
uid10804 <florian.fuss@continental.com>
2020-01-24 21:12:48 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { environment } from './../environments/environment';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angulartest';
+  data: any;
+  constructor(private http: HttpClient) {
+    console.log(environment.url);
+
+  }
+
+  async ngOnInit() {
+
+   this.data = await this.http.get(environment.url).toPromise();
+
+  }
+  
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: "https://1n4nqxuwh9.execute-api.us-east-1.amazonaws.com/dev/api/categories/"
 };
 
 /*
